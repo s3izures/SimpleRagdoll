5d63a3805790d597550835c3a8f46765d1e60977
eN <seizuresthree@gmail.com>
2024-09-08 15:38:03 -0700

    Implemented gravity and nothing else

--- SimpleRagdoll/SimpleRagdoll.cpp
+++ SimpleRagdoll/SimpleRagdoll.cpp
@@ -1,4 +1,5 @@
 #include "SimpleRagdoll.h"
+#include <iostream>
 
 void SimpleRagdoll::Main()
 {
@@ -20,20 +21,60 @@ void SimpleRagdoll::Main()
 
 void SimpleRagdoll::Start()
 {
+    //ground
+    ground = Rectangle{ 0.0f, (float)GetScreenHeight(), (float)GetScreenWidth(), 20.0f };
 
+    //Head
+    BodyPart head = BodyPart({ 50,50 }, { 200,200 }, 10, 0, 0, BLACK);
+    head.isHead = true;
+    parts.push_back(head);
+
+    //Body, bottom of head
+    BodyPart body = BodyPart({ 50,100 }, { head.position.x + head.size.x / 2, head.position.y + head.size.y }, 20, 0, 0, DARKGRAY);
+    parts.push_back(body);
 }
 
 void SimpleRagdoll::Update()
 {
-
+    Evaluate();
+    Draw();
 }
 
 void SimpleRagdoll::Evaluate()
 {
+    for (BodyPart& bp : parts)
+    {
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+            parts[0].position = GetMousePosition();
+        }
+        else if (bp.isHead)
+        {
+            if (CheckCollisionRecs(ground, bp.rec))
+            {
+                bp.totalForce = { 0.0f,0.0f };
+            }
+            else
+            {
+                //Calculate Gravity
+                bp.totalForce = Vector2{ 0, bp.mass * gravity };
+                bp.Move();
+            }
+        }
 
+        if (!bp.isHead)
+        {
+            bp.position.x = parts[0].position.x - bp.size.x;
+            bp.position.y = parts[0].position.y - bp.size.y;
+        }
+    }
 }
 
 void SimpleRagdoll::Draw()
 {
-
+    for (BodyPart& bp : parts)
+    {
+        bp.DrawPart();
+    }
 }
+
--- SimpleRagdoll/SimpleRagdoll.h
+++ SimpleRagdoll/SimpleRagdoll.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <raylib.h>
+#include <vector>
+#include <math.h>
 
 class SimpleRagdoll
 {
@@ -13,6 +15,49 @@ private:
     void Draw();
 
     const Vector2 screenSize = { 800,500 };
-    const Color clearColor = { 250, 250, 250, 255 };
+    const Color clearColor = RAYWHITE;
+
+    float gravity = 9.8f;
+
+    Rectangle ground;
+
+    struct BodyPart {
+        //=========== Variables
+        Rectangle rec;
+        Color color;
+
+        Vector2 position = { 0,0 };
+        Vector2 totalForce = { 0,0 };
+        Vector2 size;
+        float mass;
+        float friction;
+        float angle;
+
+        bool isHead = false;
+
+        //=========== Functions
+        BodyPart(Vector2 limbSize, Vector2 limbPos, float limbMass, float limbFriction, float limbAngle, Color col)
+        {
+            size = limbSize;
+            position = limbPos;
+            mass = limbMass;
+            friction = limbFriction;
+            angle = limbAngle;
+            color = col;
+        }
+
+        void DrawPart()
+        {
+            rec = Rectangle{ position.x,position.y,size.x,size.y };
+            DrawRectanglePro(rec, { 0,0 }, angle, color);
+        }
+
+        void Move()
+        {
+            position.x += totalForce.x * GetFrameTime();
+            position.y += totalForce.y * GetFrameTime();
+        }
+    };
+    std::vector<BodyPart> parts;
 };
 
